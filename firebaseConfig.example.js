e1f5c270010a92dda04cff3cbfb143b44e98d8a3
skelejarofficial <skelejarofficial@gmail.com>
2025-12-11 13:16:42 -0700

    Add example config and switch to module import

---
+++ firebaseConfig.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_DOMAIN_HERE",
+  projectId: "YOUR_PROJECT_ID_HERE",
+  storageBucket: "YOUR_BUCKET_HERE",
+  messagingSenderId: "YOUR_SENDER_ID_HERE",
+  appId: "YOUR_APP_ID_HERE"
+};
--- public/scripts.js
+++ public/scripts.js
@@ -8,16 +8,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 // Firebase Configuration
-const firebaseConfig = {
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_DOMAIN_HERE",
-  projectId: "YOUR_PROJECT_ID_HERE",
-  storageBucket: "YOUR_BUCKET_HERE",
-  messagingSenderId: "YOUR_SENDER_ID_HERE",
-  appId: "YOUR_APP_ID_HERE"
-};
-};
+import { firebaseConfig } from "./firebaseConfig.js";
 
 // Initialize Firebase Services
 const app = initializeApp(firebaseConfig);
